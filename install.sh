3e247642629faaf3ae3a91a6f05d0809f8c96834
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2021-04-22 02:06:26 +0300

    Update install.sh
    Fixed Issues

--- install.sh
+++ install.sh
@@ -3,6 +3,18 @@
 echo -e "[?] Checking...\n"
 sleep 0.3
 
+#check for gf pattern
+if [[ "$(ls ~/.gf &>/dev/null;echo $?)" -eq 2 ]]; then
+	echo [-] gf patterns not found
+	echo [?] Installing gf patterns to $HOME/.gf
+	sleep 0.1
+	mkdir ~/.gf;cd ~/.gf
+	git clone "https://github.com/1ndianl33t/Gf-Patterns" &>/dev/null
+	mv Gf-Patterns/*.json . && rm -rf Gf-Patterns/
+	echo  [+] Installed gf Patterns
+	
+fi
+
 #Check for GOPATH
 if [[ $(echo $GOPATH) = "" ]] || [[ $(echo $GOPATH) != "$HOME/go" ]]; then
 	echo [?] Go Path not setted
@@ -76,11 +88,11 @@ fi
 
 for value in "${not_installed[@]}";do
 	if [[ "$value" == *"httpx"* ]] || [[ "$value" == *"gau"* ]];then
-		$(go get -u "github.com/tomnomnom/$value")
-		$(GO111MODULE=on go get -u -v "github.com/lc/gau")
-		$(GO111MODULE=on go get -v "github.com/projectdiscovery/httpx/cmd/httpx") 	
+		$(go get -u "github.com/tomnomnom/$value" &>/dev/null)
+		$(GO111MODULE=on go get -u -v "github.com/lc/gau" &>/dev/null)
+		$(GO111MODULE=on go get -v "github.com/projectdiscovery/httpx/cmd/httpx" &>/dev/null) 	
 	else
-		$(go get -u "github.com/tomnomnom/$value")	
+		$(go get -u "github.com/tomnomnom/$value" &>/dev/null)	
 	fi
 done
 source ~/.bashrc
