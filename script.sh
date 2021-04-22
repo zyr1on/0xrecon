3f258fe5f7d0975e1e7bed3ab3821712ea449c51
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2021-04-22 07:32:18 +0300

    Update
    Updated and Fixed Issues

--- script.sh
+++ script.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 start=`date +%s`
+
+if [[ $(ls scan/ &>/dev/null;echo $?) -eq 2 ]]; then
+	mkdir scan/
+	printf "[+] Scan file created. Data will be stored there: $(pwd)/scan \n"
+else
+	printf "[!] Old Scan file found.If you do not delete it, the data will be overwritten. Delete? y/n>> "
+	read scan_data
+	if [ "$scan_data" = "yes"  ] || [ "$scan_data" = "y" ];then
+		rm -rf scan
+      		mkdir scan
+	else
+		printf "[?] Data overwriting to $(pwd)/scan \n"
+	fi
+fi
+
 printf "[?] Enter Target>> "
 read target
 printf "[?] Enter Pattern gf >> "
@@ -11,33 +26,32 @@ printf "[?] Are you want to disable httpx output with colors y/n>> "
 read answer
 
 echo -n Target: $target Pattern: $pattern Payload: $payload" | ";date 
-mkdir scan
-cd scan
+
 gau_scan() {
-	gau $target >> gau.lst #Gau tool output
+	gau $target > scan/gau.lst #Gau tool output
 }
 wayback() {
-	waybackurls $target >> waydata.lst #wayback machine output
+	waybackurls $target > scan/waydata.lst #wayback machine output
 }
 pattern() {
-	cat gau.lst waydata.lst | sort -u >> urls.lst #combine gau and wayback datas
-	cat urls.lst | gf $pattern >> $pattern.txt    #gf pattern to search for defined pattern
+	cat scan/gau.lst scan/waydata.lst | sort -u > scan/urls.lst #combine gau and wayback datas
+	cat scan/urls.lst |gf $pattern > scan/$pattern.out    #gf pattern to search for defined pattern
 }
 replace() {
-	cat $pattern.txt | qsreplace | qsreplace -a $payload >> payload.txt #add payload as placeholder
+	cat scan/$pattern.out | grep '=' | qsreplace | qsreplace $payload > scan/replaced_payload.out #add payload as placeholder
 }
 
 http_x() {
 	if [ "$answer" = "y" ] || [ "$answer" = "yes" ]; then
 		if [[ "$pattern" == *"redirect"* ]]; then
-			cat payload.txt | httpx -title -status-code -silent -csp-probe -no-color -follow-redirects  > httpx.out
+			cat scan/replaced_payload.out | httpx -title -status-code -silent -csp-probe -no-color -follow-redirects  > scan/httpx.out
 		else
-			cat payload.txt | httpx -title -status-code -silent -csp-probe -no-color > httpx.out
+			cat scan/replaced_payload.out | httpx -title -status-code -silent -csp-probe -no-color > scan/httpx.out
 		fi
 	else
-		cat payload.txt | httpx -title -status-code -silent -csp-probe -follow-redirects > httpx.out
+		cat scan/replaced_payload.out | httpx -title -status-code -silent -csp-probe -follow-redirects > scan/httpx.out
 	fi
-	cat httpx.out | awk '{print $2,$3,$NF,$1}' | sort -u > finish.out
+	cat scan/httpx.out | awk '{print $2,$3,$NF,$1}' | sort -u > scan/finish.out
 }
 gau_scan
 wayback
